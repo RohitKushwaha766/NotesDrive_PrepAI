3cd0ddfeb96be7a2fa8577387a6cd06255e88fbc
RohitKushwaha766 <kushwaharohitg555@gmail.com>
2026-07-09 08:05:12 +0530

    Make branding price configurable from backend

--- server/routes/credits.route.js
+++ server/routes/credits.route.js
@@ -3,16 +3,25 @@ import isAuth from "../middleware/isAuth.js"
 import {
   createBrandingOrder,
   createCreditsOrder,
+  claimRewardedAdCredits,
+  checkFreeDownloadUnlock,
+  debugCreditPlans,
+  getCreditPlans,
+  getBrandingConfig,
+  handleAdMobSsvReward,
   verifyBrandingPayment,
   verifyCreditsPayment
 } from "../controllers/credits.controller.js"
 
-
-
-
 const creditRouter = express.Router()
+creditRouter.get("/plans" , getCreditPlans )
+creditRouter.get("/plans/debug" , debugCreditPlans )
+creditRouter.get("/branding/config" , getBrandingConfig )
 creditRouter.post("/order" , isAuth ,createCreditsOrder )
 creditRouter.post("/verify" , isAuth ,verifyCreditsPayment )
+creditRouter.post("/reward-ad" , isAuth ,claimRewardedAdCredits )
+creditRouter.get("/admob-ssv" , handleAdMobSsvReward )
+creditRouter.get("/free-download-unlock" , checkFreeDownloadUnlock )
 creditRouter.post("/branding/order" , isAuth ,createBrandingOrder )
 creditRouter.post("/branding/verify" , isAuth ,verifyBrandingPayment )
 
