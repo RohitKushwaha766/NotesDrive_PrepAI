d8f6e415417a49e8f07c6e12b85ff373cb4ac69c
RohitKushwaha766 <158202139+RohitKushwaha766@users.noreply.github.com>
2026-07-25 12:48:43 +0530

    Add evaluateExam route to notesRouter

--- routes/genrate.route.js
+++ routes/genrate.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import { generateNotes } from "../controllers/generate.controller.js"
+import { evaluateExam, generateNotes } from "../controllers/generate.controller.js"
 import { getMyNotes, getSingleNotes } from "../controllers/notes.controller.js"
 
 
@@ -9,6 +9,7 @@ const notesRouter = express.Router()
 
 
 notesRouter.post("/generate-notes",isAuth,generateNotes)
+notesRouter.post("/evaluate-exam", isAuth, evaluateExam)
 notesRouter.get("/getnotes", isAuth,getMyNotes)
 notesRouter.get("/:id" , isAuth , getSingleNotes)
 
