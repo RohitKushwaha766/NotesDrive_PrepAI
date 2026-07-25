620c777646384c5e1b50070eee8e9ac36c00417a
RohitKushwaha766 <158202139+RohitKushwaha766@users.noreply.github.com>
2026-07-25 12:46:10 +0530

    Delete server directory

--- server/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { googleAuth, logOut, mobileAuth } from "../controllers/auth.controller.js"
-
-const authRouter = express.Router()
-
-authRouter.post("/google" , googleAuth)
-authRouter.post("/mobile" , mobileAuth)
-authRouter.get("/logout" , logOut)
-export default authRouter
